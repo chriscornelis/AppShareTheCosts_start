81d6ddb65610fc08cf97c34b4d5dd38ec45e9caf
chriscornelis <chriscornelis_10@hotmail.com>
2013-07-24 15:31:02 +0200

    lijst maken
    aanmaken van een uitgavenlijst

---
+++ lijstmaken.php
@@ -0,0 +1,14 @@
+<?php include 'header.php'; ?>
+
+	<div data-role="content">
+			<h2>Maak een uitgavenlijst</h2>
+			
+			<input type="text" name="list" id="name_list" placeholder="Naam lijst" value="">
+			<input type="password" name="password" id="password_list" placeholder="Kies een wachtwoord" value="" autocomplete="off">
+			
+			<a href="#" data-role="button" id="make_list">Maak de lijst</a>
+			
+		</div><!--content-->
+		
+	
+	<?php include 'footer.php'; ?>
