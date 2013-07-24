d9af13bd1b59362279e14771b9cfc43d79a28d46
chriscornelis <chriscornelis_10@hotmail.com>
2013-07-24 19:50:14 +0200

    footer links
    link naar pagina's om een lijst te maken en lijsten te zoeken

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
 <div data-role="footer" data-id="foo1" data-position="fixed">
 				<div data-role="navbar">
 				    <ul>
-				        <li><a href="#" data-icon="bars" class="ui-btn-active">LIJSTEN</a></li>
-				        <li><a href="#" data-icon="edit">MAAK LIJST</a></li>
-				        <li><a href="#" data-icon="search">ZOEK</a></li>
+				        <li><a href="overzichtlijsten.php" data-icon="bars" class="ui-btn-active">LIJSTEN</a></li>
+				        <li><a href="lijstmaken.php" data-icon="edit">MAAK LIJST</a></li>
+				        <li><a href="zoek.php" data-icon="search">ZOEK</a></li>
 				    </ul>
 				</div><!-- /navbar -->
 			</div><!--footer-->
