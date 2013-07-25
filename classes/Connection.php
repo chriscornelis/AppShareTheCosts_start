00c12fc5738032bd4bcebdd131e8eb0c518fd7c8
chriscornelis <chriscornelis_10@hotmail.com>
2013-07-25 12:38:52 +0200

    connection class
    klasse met connectie naar DB

---
+++ classes/Connection.php
@@ -0,0 +1,7 @@
+<?php 
+		$sHost = "localhost";
+		$sUser = "root";
+		$sPassword = "";
+		$sDatabase = "mobieleapp";
+		$link = new mysqli($sHost, $sUser, $sPassword, $sDatabase) or die("Database is gone!");	
+?>
