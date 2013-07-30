16ecd9e175c3b10d932b4bfe48c4dbb02ca78900
chriscornelis <chriscornelis_10@hotmail.com>
2013-07-30 14:58:58 +0200

    lijstmaken.php, formtags, veldinhoud ophalen met php

--- lijstmaken.php
+++ lijstmaken.php
@@ -1,13 +1,48 @@
-<?php include 'header.php'; ?>
+<?php
+include('classes/Costlist.class.php');
+	/*if(isset($_POST['make_list']))
+	{
+	$feedback = 'tot hier';
 
+		if(!empty($_POST['name_list']) && !empty($_POST['password_list']))
+		{
+			
+			try
+			{
+				$list = new List();
+				$list->ListName = $_POST['name_list'];
+				//$list->UserID = $_POST['']; ------- moet uit SESSION gehaald worden
+				$list->ListPass = $_POST['password_list'];
+				$list->SaveList();
+				
+			}
+			catch(Exception $e)
+			{
+				$feedback = $e->getMessage();
+			}
+		}
+		else
+		{
+			$feedback = "Vergeet niet alle velden in te vullen";
+		}
+	}*/
+?>	
+<?php include 'header.php'; ?>
 	<div data-role="content">
 			<h2>Maak een uitgavenlijst</h2>
+			<form action="" method="post">
+				<input type="text" name="list" id="name_list" placeholder="Naam lijst" value="">
+				<input type="password" name="password" id="password_list" placeholder="Kies een wachtwoord" value="" autocomplete="off">
+				
+				<input type="submit" name="make_list" id="make_list" value="Maak de lijst" />
+			</form>
 			
-			<input type="text" name="list" id="name_list" placeholder="Naam lijst" value="">
-			<input type="password" name="password" id="password_list" placeholder="Kies een wachtwoord" value="" autocomplete="off">
+			<?php if(isset($feedback)):?>
+			<div class="feedback">
 			
-			<a href="#" data-role="button" id="make_list">Maak de lijst</a>
-			
+			<?php echo $feedback; ?>
+			</div>
+			<?php endif; ?>
 		</div><!--content-->
 		
 	
