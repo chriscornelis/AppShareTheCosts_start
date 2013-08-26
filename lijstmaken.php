eb5b00345c6f7c674ead77e4143761dd938e0b20
chriscornelis <chriscornelis_10@hotmail.com>
2013-08-26 13:49:36 +0200

    Lijstmaken.php Benamingen veranderd

--- lijstmaken.php
+++ lijstmaken.php
@@ -1,20 +1,20 @@
 <?php
 include('classes/Costlist.class.php');
-	/*if(isset($_POST['make_list']))
+	if(isset($_POST['make_list']))
 	{
-	$feedback = 'tot hier';
-
+	
 		if(!empty($_POST['name_list']) && !empty($_POST['password_list']))
 		{
 			
 			try
 			{
-				$list = new List();
+				$list = new Costlist();
 				$list->ListName = $_POST['name_list'];
 				//$list->UserID = $_POST['']; ------- moet uit SESSION gehaald worden
 				$list->ListPass = $_POST['password_list'];
-				$list->SaveList();
-				
+				$list->Savelist();
+				//$feedback = 'tot hier';
+
 			}
 			catch(Exception $e)
 			{
@@ -25,14 +25,14 @@ include('classes/Costlist.class.php');
 		{
 			$feedback = "Vergeet niet alle velden in te vullen";
 		}
-	}*/
+	}
 ?>	
 <?php include 'header.php'; ?>
 	<div data-role="content">
 			<h2>Maak een uitgavenlijst</h2>
 			<form action="" method="post">
-				<input type="text" name="list" id="name_list" placeholder="Naam lijst" value="">
-				<input type="password" name="password" id="password_list" placeholder="Kies een wachtwoord" value="" autocomplete="off">
+				<input type="text" name="name_list" id="name_list" placeholder="Naam lijst" value="">
+				<input type="password" name="password_list" id="password_list" placeholder="Kies een wachtwoord" value="" autocomplete="off">
 				
 				<input type="submit" name="make_list" id="make_list" value="Maak de lijst" />
 			</form>
